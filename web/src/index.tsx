6708bc213110932b5623abf00715cf75cb22a08f
tsunoda1 <tsunoda@b2ml.com.br>
2020-11-25 19:45:11 -0300

    fix: change eslint version

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -3,11 +3,9 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-
 ReactDOM.render(
   <StrictMode>
     <App />
   </StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
-
